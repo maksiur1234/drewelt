5d079123d6c31b67f180e065497cd6034054219a
Wormsonn <maksiur1234@gmail.com>
2025-02-06 22:32:17 +0100

    fix moda

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,20 +1,31 @@
 import { Button } from "@heroui/button";
-import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@heroui/modal";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@heroui/modal";
 import React from "react";
 
+type Backdrop = "opaque" | "blur";
 
-export default function BlurModal({name, description, label}:
-    {
-    name: string,
-    description: string,
-    label: string
-    }) {
-  const {isOpen, onOpen, onClose} = useDisclosure();
-  const [backdrop, setBackdrop] = React.useState("opaque");
+export default function BlurModal({
+  name,
+  description,
+  label,
+}: {
+  name: string;
+  description: string;
+  label: string;
+}) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [backdrop, setBackdrop] = React.useState<Backdrop>("opaque");
 
-  const backdrops = ["blur"];
+  const backdrops: Backdrop[] = ["blur"];
 
-  const handleOpen = (backdrop) => {
+  const handleOpen = (backdrop: Backdrop) => {
     setBackdrop(backdrop);
     onOpen();
   };
@@ -36,20 +47,18 @@ export default function BlurModal({name, description, label}:
       </div>
       <Modal backdrop={backdrop} isOpen={isOpen} onClose={onClose}>
         <ModalContent>
-          {(onClose) => (
+          {(onClose: () => void) => (
             <>
               <ModalHeader className="flex flex-col gap-1">{name}</ModalHeader>
               <ModalBody>
-                <p>
-                  {description}
-                </p>
+                <p>{description}</p>
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
                   Zamknij
                 </Button>
                 <Button color="primary" onPress={onClose}>
-                  Sczegóły
+                  Szczegóły
                 </Button>
               </ModalFooter>
             </>
@@ -59,4 +68,3 @@ export default function BlurModal({name, description, label}:
     </>
   );
 }
-

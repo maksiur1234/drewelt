5ee6aa6363f3e75292cea8a64e41c243348edfaf
Maksymilian Robak <maksiur1234@gmail.com>
2025-07-25 14:47:47 +0200

    port smtp

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -12,7 +12,7 @@ export async function POST(request) {
 
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT) || 587,
+      port: process.env.SMTP_PORT || 465,
       secure: true,
       auth: {
         user: process.env.SMTP_USER,

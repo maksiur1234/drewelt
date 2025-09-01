cc6463887f85a9f857f1bd0d71d32daed8cdac5d
Pikseo <techseo@pikseogroup.pl>
2025-09-01 09:28:55 +0200

    Poprawa oddalenia

--- components/googleMaps.tsx
+++ components/googleMaps.tsx
@@ -3,7 +3,7 @@
 export const GoogleMaps = () => {
   return (
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8540.899882903635!2d17.027405682734706!3d52.558495117961655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4704672d890aa563%3A0xe77143c9245062bb!2sPliszki%2018%2C%2062-095%20Rakownia!5e0!3m2!1spl!2spl!4v1756709237773!5m2!1spl!2spl&z=14"
+      src="https://www.google.com/maps?q=52.558495117961655,17.027405682734706&z=13&output=embed"
       width="100%"
       height={450}
       style={{ border: 0 }}
